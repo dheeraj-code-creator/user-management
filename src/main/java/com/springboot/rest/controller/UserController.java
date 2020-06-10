70a6f57c4a310f342a632dfb8e6977b4113aba49
dheeraj-code-creator <54579746+dheeraj-code-creator@users.noreply.github.com>
2020-06-10 21:30:31 +0530

    Update UserController.java
    updated

--- src/main/java/com/springboot/rest/controller/UserController.java
+++ src/main/java/com/springboot/rest/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class UserController {
 	@GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public UserDto getUserById(@PathVariable("userId") String userId) {
 		return userService.getUserByUserId(userId);
+	}
+	
+	// delete users
+	@DeleteMapping(value = "/delete/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public void deleteUser(@PathVariable(value = "userId") String userId) {
+		userService.deleteById(userId);
 	}
 
 }
